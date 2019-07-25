687e400a78c8eeb4fab6fdfa8a65f2ee9ac5634a
Thai Nguyen <discoverylight@yahoo.com>
2019-07-25 09:07:24 -0700

    mvp complete

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import axios from "axios";
 
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
-
-import "./styles.css";
+import "bulma";
+import "./styles.scss";
 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
