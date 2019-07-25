630962d87a9aa01845a884028777b5d6f2da4ff0
Thai Nguyen <discoverylight@yahoo.com>
2019-07-25 10:36:25 -0700

    refactored useEffect hook in index.js to a useCoinGecko.js

--- src/index.js
+++ src/index.js
@@ -2,22 +2,25 @@ import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
 
+
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
 import "bulma";
 import "./styles.scss";
+import useCoinGecko from "./hooks/useCoinGecko";
 
 const App = () => {
-  const [coinData, setCoinData] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
-      )
-      .then(res => setCoinData(res.data))
-      .catch(err => console.log(err));
-  }, []);
+  const [coinData, setCoinData] = useCoinGecko();
+  console.log(coinData);
+  
+  // useEffect(() => {
+  //   axios
+  //     .get(
+  //       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
+  //     )
+  //     .then(res => setCoinData(res.data))
+  //     .catch(err => console.log(err));
+  // }, []);
   return (
     <div className="App">
       <Navbar />
